7ade41d22804cc63a555c1d91f2f765f26efa735
filipe mota <filipe.dmtm@puccampinas.edu.br>
2024-10-18 19:02:21 -0300

    Implementando a classe Avaliação

---
+++ src/main/java/org/example/models/Avaliacao.java
@@ -0,0 +1,60 @@
+package org.example.models;
+
+public class Avaliacao {
+    private String user;
+    private String company;
+    private String comment;
+    private  int grade;
+
+    public Avaliacao(String username, String company, String comment, int grade){
+        this.user = username;
+        this.company = company;
+        this.comment = comment;
+        this.grade = grade;
+    }
+
+    public String getUser(){
+        return this.user;
+    }
+
+    public String getCompany(){
+        return this.company;
+    }
+
+    public String getComment(){
+        return this.comment;
+    }
+
+    public int getGrade(){
+        return this.grade;
+    }
+
+    @Override
+    public boolean equals(Object b){
+        if(b == null) return false;
+        if(b == this) return true;
+        if(b.getClass()!= this.getClass()) return false;
+
+        Avaliacao other = (Avaliacao) b;
+        if(!this.user.equals(other.user)) return false;
+        if(!this.company.equals(other.company)) return false;
+        if(!this.comment.equals(other.comment)) return false;
+        if(this.grade != other.grade) return false;
+        return true;
+    }
+
+    @Override
+    public int hashCode(){
+        int ret = 1;
+        ret = ret * 7 + this.user.hashCode();
+        ret = ret * 7 + this.company.hashCode();
+        ret = ret * 7 + this.comment.hashCode();
+        ret = ret * 7 + Integer.valueOf(this.grade).hashCode();
+        return ret;
+    }
+
+    @Override
+    public String toString(){
+        return(this.user+"/"+this.company+"/"+this.comment+"/"+this.grade);
+    }
+}
